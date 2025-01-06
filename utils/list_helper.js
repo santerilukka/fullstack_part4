84f4fc7a0dfbbdcb48d63c66f935f62227ad6f6e
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-01-06 20:24:27 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
   }
 
+const favoriteBlog = (blogs) => {
+if (blogs.length === 0) return null
+
+const favorite = blogs.reduce((prev, current) => 
+    current.likes > prev.likes ? current : prev
+)
+
+return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+}}
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
