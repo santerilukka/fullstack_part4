d84a07de36ff5b1400006c5b5d8109ce87b6e04f
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-01-07 01:46:53 +0200

    4.10: blogilistan testit, step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -35,6 +35,27 @@ test('there are the right amount of blogs', async () => {
     })
   })
 
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Async/Await Simplifies Making Async Calls',
+      author: 'John Doe',
+      url: 'https://example.com/async-await',
+      likes: 10,
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+  
+    const titles = blogsAtEnd.map((b) => b.title)
+    assert.ok(titles.includes(newBlog.title))
+  })
+
 after(async () => {
   await mongoose.connection.close()
 })
