1ac62834c31a35e57e838f9b804ba71e5b84f43c
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-01-10 14:42:03 +0200

    4.19: blogilistan laajennus, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,16 @@
 const blogsRouter = require('express').Router()
 const { default: mongoose } = require('mongoose')
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+      return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,7 +19,11 @@ blogsRouter.get('/', async (request, response) => {
   
   blogsRouter.post('/', async (request, response) => {
       const body = request.body
-      const user = await User.findById(body.userId)
+      const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+      if(!decodedToken.id){
+        return response.status(401).json({ error: 'token invalid' })
+      }
+      const user = await User.findById(decodedToken.id)
 
       const blog = new Blog({
         title: body.title,
