9c604d4a0c8f8ee5a776f07b87809e7e7b27d5e4
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-01-10 15:16:20 +0200

    4.20*: blogilistan laajennus, step8

--- utils/middleware.js
+++ utils/middleware.js
@@ -12,6 +12,16 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -31,5 +41,6 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
